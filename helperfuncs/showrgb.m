82c0a807dcb94d3632e7537384d6539d84f51e7c
Devin Sullivan <devinsullivan@DevinSiMac.scilifelab.se>
2016-09-29 14:25:07 +0200

    added functionality to showrgb.m

--- helperfuncs/showrgb.m
+++ helperfuncs/showrgb.m
@@ -11,11 +11,41 @@ end
 if isstr(b_img)
     b_img = imread(b_img);
 end
+%fill missing channels
+%missing r
+if isempty(r_img)
+    if ~isempty(g_img)
+        r_img = zeros(size(g_img));
+    else
+        r_img = zeros(size(b_img));
+    end
+end
+%missing g
+if isempty(g_img)
+    if ~isempty(r_img)
+        g_img = zeros(size(r_img));
+    else
+        g_img = zeros(size(b_img));
+    end
+end
+%missing b
+if isempty(b_img)
+    if ~isempty(r_img)
+        b_img = zeros(size(r_img));
+    else
+        b_img = zeros(size(g_img));
+    end
+end
+
 
 imgrgb = zeros(size(r_img,1),size(r_img,2),3);
 
 imgrgb(:,:,1) = double(r_img)./max(double(r_img(:)));
-imgrgb(:,:,2) = double(g_img)./max(double(r_img(:)));
-imgrgb(:,:,3) = double(b_img)./max(double(r_img(:)));
+imgrgb(:,:,2) = double(g_img)./max(double(g_img(:)));
+imgrgb(:,:,3) = double(b_img)./max(double(b_img(:)));
 
 imshow(imgrgb,[])
+
+if nargout==0
+    imgrgb = [];
+end
