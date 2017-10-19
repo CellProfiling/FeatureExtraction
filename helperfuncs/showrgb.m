90260253809dad018d5cee8e602521e400eaa870
devins <devsullivan@gmail.com>
2017-10-19 17:01:36 +0200

    added functionality to helper function for viewing rgb images easily. Does not impact production.

--- helperfuncs/showrgb.m
+++ helperfuncs/showrgb.m
@@ -6,14 +6,32 @@ if nargin<4
 end
 
 %if not images, load them
-if isstr(r_img)
+if ischar(r_img)
     r_img = imread(r_img);
+elseif iscell(r_img)
+    r_imgtmp = double(imread(r_img{1}));
+    for i = 2:length(r_img)
+        r_imgtmp = r_imgtmp+double(imread(r_img{i}));
+    end
+    r_img = r_imgtmp;
 end
-if isstr(g_img)
+if ischar(g_img)
     g_img = imread(g_img);
+elseif iscell(g_img)
+    g_imgtmp = double(imread(g_img{1}));
+    for i = 2:length(g_img)
+        g_imgtmp = g_imgtmp+double(imread(g_img{i}));
+    end
+    g_img = g_imgtmp;
 end
-if isstr(b_img)
+if ischar(b_img)
     b_img = imread(b_img);
+elseif iscell(b_img)
+    b_imgtmp = double(imread(b_img{1}));
+    for i = 2:length(b_img)
+        b_imgtmp = b_imgtmp+double(imread(b_img{i}));
+    end
+    b_img = b_imgtmp;
 end
 %fill missing channels
 %missing r
@@ -47,6 +65,7 @@ imgrgb = zeros(size(r_img,1),size(r_img,2),3);
 imgrgb(:,:,1) = double(r_img)./max(double(r_img(:)));
 imgrgb(:,:,2) = double(g_img)./max(double(g_img(:)));
 imgrgb(:,:,3) = double(b_img)./max(double(b_img(:)));
+%imgrgb(:,:,3) = double(b_img)./(quantile(double(b_img(:)),0.99));
 
 if show
     imshow(imgrgb,[])
